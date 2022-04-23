697b6427ce0724b64e125bed1fe9eaf4e412cbb7
William Casey <will.casey5@gmail.com>
2022-04-23 17:15:00 -0600

    user interface and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,73 @@
-import React, { ReactNode } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import UserCard from './components/userCard';
 
 function App() {
+  const [usersData, setUsersData] = useState([]);
+  const [fetchError, setFetchError] = useState('');
 
-  let cards: Array<ReactNode> = [];
-  for (let i = 0; i < 10; i++) {
-    cards.push(<UserCard key={i}></UserCard>);
+  const fetchUserData = async () => {
+    try {
+      // Use latest comic number to get total xkcd count
+      // IMPORTANT: Proxying for CORS purposes. Please visit https://cors-anywhere.herokuapp.com/corsdemo
+      //            and click the "Request temporary access to the demo server" button before running the app
+      const xkcdPromise = fetch('https://cors-anywhere.herokuapp.com/https://xkcd.com/info.0.json');
+      const usersPromise = fetch('https://randomuser.me/api/?results=10');
+
+      const [usersResponse, xkcdResponse] = await Promise.all([usersPromise, xkcdPromise]);
+      const [usersRes, xkcdRes] = await Promise.all([usersResponse.json(), xkcdResponse.json()])
+
+      // get random xkcd number from total range
+      const xkcdCount = xkcdRes.num;
+
+      const usersData = usersRes.results.map((userRes: any) => {
+        userRes.xkcd = Math.floor(Math.random() * xkcdCount);
+        return userRes;
+      });
+    
+      setUsersData(usersData);
+    } catch (e) {
+      console.error(e);
+
+      // If the proxy didn't work, retry with static count for XKCD
+      try {
+        console.log('CORS proxy failed, fetching with static XKCD count');
+        const usersResponse = await fetch('https://randomuser.me/api/?results=10');
+        const usersRes = await usersResponse.json();
+        const xkcdCount = 2610;
+
+        const usersData = usersRes.results.map((userRes: any) => {
+          userRes.xkcd = Math.floor(Math.random() * xkcdCount);
+          return userRes;
+        });
+        
+        setUsersData(usersData);
+      } catch (e) {
+        console.error(e);
+        setFetchError('Could not fetch data');
+      }
+    }
   }
 
+  useEffect(() => {
+    fetchUserData();
+  }, [])
+
   return (
-    <div className="content">
-      {
-        cards.map(card => {
-          return card;
-        })
-      }
-    </div>
+    <>
+      <header>
+        Symplr Challenge
+      </header>
+      <main>
+        {(!fetchError) ?
+          usersData.map((userData, i) => {
+            return <UserCard key={i} data={userData}/>
+          })
+        : 
+          <div className="error">{fetchError}</div>
+        }
+      </main>
+    </>
   );
 }
 
--- src/components/userCard.tsx
+++ src/components/userCard.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import './userCard.css';
 
-function UserCard() {
-  return (
-    <div className="user-card">UserCard</div>
-  )
+function UserCard({data}: {data: any}) {
+    //console.log(data);
+    const fullName = data.name.first + ' ' + data.name.last;
+    return (
+      <div className="user-card">
+            <div className="user-profile">
+                <div className="image-container">
+                    <img src={data.picture.medium} alt={fullName} />
+                    </div>
+                <div className="info-container">
+                    <div className="name">{fullName}</div>
+                    <div className="gender">{data.gender}</div>
+                    <div className="email"><a href={'mailto:' + data.email}>{data.email}</a></div>
+                    <div className="address">
+                        <span>{data.location.street.number + ' ' + data.location.street.name + ', '}</span>
+                        <span>{data.location.city + ', ' + data.location.state + ' ' + data.location.postcode + ', '}</span>
+                        <span>{data.location.country}</span>
+                    </div>
+                    <div className="xkcd">
+                        <a href={'https://xkcd.com/' + data.xkcd} target="_blank" rel="noreferrer">Relevant XKCD</a>
+                    </div>
+                </div>
+            </div>
+            <div className="button-container">
+                <a href={'tel:' + data.phone}>
+                    <button className="phone">{data.phone}</button>
+                </a>
+            </div>
+      </div>
+    )
 }
 
 export default UserCard
